e8cc4d316633084ad4282c6982ef226312a4ffb7
Cary FitzHugh <cary.fitzhugh@gmail.com>
2019-02-27 13:44:12 -0500

    fixes header... ?" g

--- src/header.js
+++ src/header.js
@@ -13,23 +13,29 @@ class Header extends Component {
  render() {
    return (
      <div>
-      <nav className='navbar navbar-toggleable-md navbar-inverse bg-inverse'>
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <Link to="/" className='navbar-brand'> NDS</Link>
-        <div className='collapse navbar-collapse' id='navbarNav'>
-          <ul className="navbar-nav mr-auto">
-            <HeaderLink path_prefix='/resources' path='/resources' title='Resources' />
-            <HeaderLink path_prefix='/collections' path='/collections' title='Collections' />
-            <HeaderLink path_prefix='/geofocuses' path='/geofocuses' title='Geofocuses' />
-            <HeaderLink path_prefix='/history' path='/history' title='History' />
-            <span className='nav-item'>
-              <a href="/sign_out" className='nav-link'> <span className='fa fa-sign-out'></span> Logout</a>
-            </span>
-          </ul>
-        </div>
-      </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <Link to="/" className='navbar-brand'> NDS</Link>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                  <HeaderLink path_prefix='/resources' path='/resources' title='Resources' />
+                  <HeaderLink path_prefix='/collections' path='/collections' title='Collections' />
+                  <HeaderLink path_prefix='/geofocuses' path='/geofocuses' title='Geofocuses' />
+                  <HeaderLink path_prefix='/history' path='/history' title='History' />
+                  <li>
+                    <span className='nav-item'>
+                      <a href="/sign_out" className='nav-link'> <span className='fa fa-sign-out'></span> Logout</a>
+                    </span>
+                  </li>
+            </ul>
+          </div>
+        </nav>
+
+
+
       <Switch>
         <Route path='/resources' component={ResourcesSubnav}/>
         <Route path='/collections' component={CollectionsSubnav}/>
